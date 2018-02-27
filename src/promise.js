179a7dd5abe17c5f6c51e6913a376c76fa84804d
zymseo <1047832475@qq.com>
2018-02-27 22:34:13 +0800

    将Promise构造函数内的resolve方法是异步执行，并引入status状态值，确保异步获取到结果之后通过then注册的回调函数能全部执行

--- src/promise.js
+++ src/promise.js
@@ -16,33 +16,41 @@
     return Object.prototype.toString.call(arry) === '[object Array]';
   }
 
+  function timer (self, value, fn) {
+    var timeout = setTimeout(function () {
+      fn(self, value);
+      clearTimeout(timeout);
+      timeout = null;
+    }, 0);
+  }
+
   function resolve (self, value) {
     var resolves = self.handles.resolves,
       len = resolves.length;
-    if (len) {
-      for (var i = 0; i < len; i += 1) {
-        resolves[i].call(self, value);
-      }
+    self.status === 0 && (self.status = 1) && (self.value = value);
+    if (len === 0) {return false;}
+    for (var i = 0; i < len; i += 1) {
+      resolves[i].call(self, value);
     }
   }
 
   function reject (self, reason) {
     var rejects = self.handles.rejects,
       len = rejects.length;
-    if (len) {
-      for (var i = 0; i < len; i += 1) {
-        rejects[i].call(self, reason);
-      }
+    self.status === 0 && (self.status = 2) && (self.value = reason);
+    if (len === 0) {return false;}
+    for (var i = 0; i < len; i += 1) {
+      rejects[i].call(self, reason);
     }
   }
 
   function doResolve (self, fn) {
     fn(function (value) {
       self.handles.rejects.length = 0;
-      resolve(self, value);
+      timer(self, value, resolve);
     }, function (reason) {
       self.handles.resolves.length = 0;
-      reject(self, reason);
+      timer(self, reason, reject);
     });
   }
 
@@ -50,20 +58,19 @@
     var successRes = [],
       errorRes = [],
       len = promiseList.length;
-    if (len) {
-      for (var i = 0; i < len; i += 1) {
-        promiseList[i].then(function (val) {
-          successRes.push(val);
-          if (successRes.length === promiseList.length) {
-            resolve(successRes);
-          }
-        }, function (val) {
-          if (errorRes.length === 0) {
-            errorRes.push(val);
-            reject(val);
-          }
-        });
-      }
+    if (len === 0) {return false;}
+    for (var i = 0; i < len; i += 1) {
+      promiseList[i].then(function (val) {
+        successRes.push(val);
+        if (successRes.length === len) {
+          resolve(successRes);
+        }
+      }, function (val) {
+        if (errorRes.length === 0) {
+          errorRes.push(val);
+          reject(val);
+        }
+      });
     }
   }
 
@@ -72,6 +79,8 @@
       resolves: [],
       rejects: []
     };
+    this.status = 0; // 0: pending 1: resolve 2: reject
+    this.value = null;
     doResolve(this, fn);
   }
 
@@ -80,17 +89,32 @@
       throw Error('The param of Promise.all must be a Array!');
     }
     return new Promise(function (resolve, reject) {
-      setTimeout(function () {
+      var timer = setTimeout(function () {
         multiplePromise(promiseList, resolve, reject);
+        clearTimeout(timer);
+        timer = null;
       }, 0);
     });
   };
 
   Promise.prototype.then = function (resolve, reject) {
-    typeof resolve === 'function' && this.handles.resolves.push(resolve);
-    typeof reject === 'function' && this.handles.rejects.push(reject);
+    switch (this.status) {
+      case 0:
+        addQueue(this, resolve, reject);
+        break;
+      case 1:
+        resolve(this.value);
+        break;
+      default:
+        reject(this.value);
+    }
     return this;
   };
+
+  function addQueue (self, resolve, reject) {
+    typeof resolve === 'function' && self.handles.resolves.push(resolve);
+    typeof reject === 'function' && self.handles.rejects.push(reject);
+  }
 
   return Promise;
 });
